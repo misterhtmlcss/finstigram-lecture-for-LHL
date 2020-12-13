b27039dd16d7586652d2950bcc5d2eae20b04d35
Roger K <misterhtmlcss@gmail.com>
2020-12-13 22:23:02 +0000

    test with class changes before second push to heroku

--- public/js/lib.js
+++ public/js/lib.js
@@ -1,12 +1,22 @@
+console.log('connected.....')
+
+
+// Finds all the a tags inside the login-info class
 let loginText = document.querySelectorAll('.login-info a')
 
-
 if(loginText.length === 1) {
+
   loginText = loginText[0]
   
+  // 'J' instead 'j'
   const firstLetter = loginText.textContent.split('')[0].toUpperCase()
+  // Original username that is now an array ['j', 'a', 'h'....]
   let prevText = loginText.textContent.split('')
+
+  // ['J', 'a', 'h'....]
   prevText.splice(0,1, firstLetter)
+
+  // 'Jah'
   loginText.textContent = prevText.join('')
   
   
